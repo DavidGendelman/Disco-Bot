f5f27fbd16264e05e953b073b87236b71216f9be
VaidD <davidgen123@outlook.com>
2021-08-03 13:44:18 -0400

    adds member identification (broken)
    member identification not working properly.

--- icarly.py
+++ icarly.py
@@ -3,7 +3,7 @@
 ##File Name: icarly.py
 ##Program Name: icarly.py
 ##Creation Date: August 2nd, 2021
-##Modified Date: August 2nd, 2021
+##Modified Date: August 3rd, 2021
 ##Description: Discord bot for discord
 
 import os
@@ -13,11 +13,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+GUILD= os.getenv('DISCORD_GUILD')
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(f'{client.user} has connected to Discord!'
+           f'{guild.name}(id: {guild.id})')
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+    print(f'test')
 
 client.run(TOKEN)
